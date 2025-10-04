3d766ee70022b7bc77d7866ab8147d14c0fcc237
FabianCaamalHaas <fcaamal@3code.us>
2025-10-04 13:08:19 -0600

    Instalacion de tailwind

--- projects/main/src/views/AboutView.vue
+++ projects/main/src/views/AboutView.vue
@@ -1,15 +1,5 @@
 <template>
-  <div class="about">
+  <div class="bg-red-700 p-4 rounded-2xl text-white mt-4">
     <h1>This is an about page</h1>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
